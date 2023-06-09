33ff55d548a56c15535c5528eb2c4b0db551772c
Gustavo Honorato Nicolau <46831966+Gushono@users.noreply.github.com>
2023-06-09 01:30:30 -0300

    test: adding unit testing to success case of service

--- tests/test_service.py
+++ tests/test_service.py
@@ -2,7 +2,8 @@ from unittest.mock import MagicMock, AsyncMock
 
 import pytest
 
-from scrapper.service import ScrapingService, CompanyDetails
+from src.services.scrapper.service import ScrapingService
+from src.dtos.dtos import CompanyDetails
 
 
 @pytest.mark.asyncio
@@ -13,19 +14,14 @@ async def test_scrape_companies_from_urls_success():
     # Create an instance of the ScrapingService with the mock scraper
     service = ScrapingService(scraper_mock)
 
-    # Define test data
     urls = ["https://example.com/companyA", "https://example.com/companyB"]
 
-    # Create a mock for the get_scraped_data method
     service.get_scraped_data = AsyncMock(return_value=[
         CompanyDetails(name="Company A", url="https://example.com/companyA"),
         CompanyDetails(name="Company B", url="https://example.com/companyB")
     ])
 
-    # Run the scraping method
     results = await service.scrape_companies_from_urls(urls)
-
-    # Assertions
     assert len(results) == 2
     assert isinstance(results[0], CompanyDetails)
     assert isinstance(results[1], CompanyDetails)
