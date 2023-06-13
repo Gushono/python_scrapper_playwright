1a597148d45aa4c463ccef94e568b7685aa99a29
Gustavo Honorato Nicolau <46831966+Gushono@users.noreply.github.com>
2023-06-13 10:51:32 -0300

    feat: adding news Dtos to improve code readability

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ ENVIRONMENT = "development" if os.environ.get("PORT") is None else "production"
 DEBUG = ENVIRONMENT == "development"
 
 app = create_app(debug_mode=DEBUG)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
--- src/dtos/dtos.py
+++ src/dtos/dtos.py
@@ -3,10 +3,13 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class CompanyPercentageStarReviews(BaseModel):
+    star: str
+    percentage: str
+
+
 class CompanyDetails(BaseModel):
-    title: Optional[str]
     description: Optional[str]
     url: str
-    links: Optional[list[str]]
-    table_data: Optional[list[dict]]
-    screenshot: Optional[str]
+    rating: Optional[str]
+    percentage_of_all_star_reviews: Optional[list[CompanyPercentageStarReviews]]
