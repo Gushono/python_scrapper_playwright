7696649d69bcbcc6e26280c0da3e29ab4b4f6b8e
Gustavo Honorato Nicolau <46831966+Gushono@users.noreply.github.com>
2023-06-13 23:38:21 -0300

    chore: changing where uvicorn is imported

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+import uvicorn
+
 from server import create_app
 
 ENVIRONMENT = "development" if os.environ.get("PORT") is None else "production"
@@ -8,6 +10,4 @@ DEBUG = ENVIRONMENT == "development"
 app = create_app(debug_mode=DEBUG)
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8080)
